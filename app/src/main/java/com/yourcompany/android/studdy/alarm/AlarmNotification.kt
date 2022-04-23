6d52f15c769d3490846a11217aa7db41cd224a12
Denis Buketa <denis.buketa@gmail.com>
2022-04-23 17:35:58 +0200

    Stop alarm sound when dismissing notification

--- app/src/main/java/com/yourcompany/android/studdy/alarm/AlarmNotification.kt
+++ app/src/main/java/com/yourcompany/android/studdy/alarm/AlarmNotification.kt
@@ -37,12 +37,14 @@ package com.yourcompany.android.studdy.alarm
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.yourcompany.android.R
 import com.yourcompany.android.studdy.MainActivity
+import com.yourcompany.android.studdy.StuddyApplication
 
 fun showNotification(
     context: Context,
@@ -51,15 +53,29 @@ fun showNotification(
     notificationId: Int,
     contentTitle: String
 ) {
-  val intent = Intent(context, MainActivity::class.java)
-  val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+  val startAppIntent = Intent(context, MainActivity::class.java)
+  val startAppPendingIntent = PendingIntent.getActivity(
+      context,
+      0,
+      startAppIntent,
+      PendingIntent.FLAG_IMMUTABLE
+  )
+
+  val deleteIntent = Intent(context, AlarmNotificationDismissedBroadcastReceiver::class.java)
+  val deletePendingIntent = PendingIntent.getBroadcast(
+      context,
+      0,
+      deleteIntent,
+      PendingIntent.FLAG_IMMUTABLE
+  )
 
   val notificationBuilder = NotificationCompat.Builder(context, channelId)
       .setSmallIcon(R.drawable.splash_icon)
       .setContentTitle(contentTitle)
       .setPriority(NotificationCompat.PRIORITY_HIGH)
       .setCategory(NotificationCompat.CATEGORY_ALARM)
-      .setFullScreenIntent(pendingIntent, true)
+      .setFullScreenIntent(startAppPendingIntent, true)
+      .setDeleteIntent(deletePendingIntent)
   val notification = notificationBuilder.build()
   val notificationManager = context.getSystemService(NotificationManager::class.java)
 
@@ -76,4 +92,13 @@ fun showNotification(
   }
 
   notificationManager.notify(notificationId, notification)
+}
+
+class AlarmNotificationDismissedBroadcastReceiver : BroadcastReceiver() {
+
+  override fun onReceive(context: Context, intent: Intent) {
+    val alarmRingtoneState = (context.applicationContext as StuddyApplication).alarmRingtoneState
+    alarmRingtoneState.value?.stop()
+    alarmRingtoneState.value = null
+  }
 }
